186314c28ccac7f5147a8af574938bed2070af97
Telson <1766965235@qq.com>
2026-07-15 12:01:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import os
 STOCKS = [
     {"code": "sz000975", "name": "山金国际", "target": 17.0, "condition": "below"},
     {"code": "sz000975", "name": "山金国际", "target": 30.0, "condition": "above"},
-    {"code": "sz159530", "name": "机器人ETF", "target": 1.2, "condition": "below"},
-    {"code": "sh603259", "name": "药明康德", "target": 145.0, "condition": "above"}
+    {"code": "sz159530", "name": "机器人ETF", "target": 1.58, "condition": "above"},
+    {"code": "sh603259", "name": "药明康德", "target": 124.0, "condition": "below"}
 ]
 
 MAIL_HOST = "smtp.qq.com"
